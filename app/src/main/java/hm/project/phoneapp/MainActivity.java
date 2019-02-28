13dc300d5e9f9818e423992b34a0097f12fd14a3
1234hemant1234 <hemantmalhotra780@gmail.com>
2019-02-28 12:36:54 +0530

    complete the app

--- app/src/main/java/hm/project/phoneapp/MainActivity.java
+++ app/src/main/java/hm/project/phoneapp/MainActivity.java
@@ -1,5 +1,7 @@
 package hm.project.phoneapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+     EditText text;
+
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,16 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                public void onClick(View view) {
+                    text = findViewById(R.id.editText);
+
+                    number=text.getText().toString().trim();
+
+                    String uri = "tel:%s" +number.trim() ;
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse(uri));
+                    startActivity(intent);
+                }
             }
         });
     }
